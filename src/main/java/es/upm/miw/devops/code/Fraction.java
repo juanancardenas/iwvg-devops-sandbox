150382dfa2afb464099a95238430ade69cc75d8f
jacardenas <juanancardenas@gmail.com>
2025-09-25 19:18:35 +0200

    add Fraction class issue#4

---
+++ src/main/java/es/upm/miw/devops/code/Fraction.java
@@ -0,0 +1,72 @@
+package es.upm.miw.devops.code;
+
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
+
+    public boolean isProper() {
+        return Math.abs(this.numerator) < Math.abs(this.denominator);
+    }
+
+    public boolean isImproper() {
+        return !this.isProper();
+    }
+
+    public boolean isEquivalent(Fraction other) {
+        return this.numerator * other.denominator == this.denominator * other.numerator;
+    }
+
+    public Fraction add(Fraction other) {
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction multiply(Fraction other) {
+        return new Fraction(this.numerator * other.numerator, this.denominator * other.denominator);
+    }
+
+    public Fraction divide(Fraction other) {
+        if (other.numerator == 0) {
+            throw new ArithmeticException("Cannot divide by zero fraction");
+        }
+        return new Fraction(this.numerator * other.denominator, this.denominator * other.numerator);
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
+}
